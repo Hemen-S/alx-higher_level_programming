46ccc15e5eacfe8b0e8a4964a5b74555d3c6e0c3
Hemen-S <hemensol1515@gmail.com>
2022-06-02 10:10:04 -0700

    edit

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -14,5 +14,12 @@
 typedef struct listint_s
 {
 	int n;
-	struct listint_
-    
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
